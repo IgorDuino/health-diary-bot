3865e7bd464727c47996907ba4b00b73cb893304
igor kuzmenkov <me@igorduino.ru>
2023-08-31 13:09:59 +0300

    do small spreadsheet refactor

--- transfer.py
+++ transfer.py
@@ -4,12 +4,14 @@ from diary.models import Dish
 df = pd.read_excel("dishes.xlsx")
 
 for index, row in df.iterrows():
-    dish = Dish(
-        title=row["Продукт"][6:],
-        grams=row["Вес, г"],
-        calories=row["Калорийность, ккал"],
-        protein=row["Белок, г"],
-        fats=row["Жир общий, г"],
-        carbohydrates=row["Углеводы, по разности, г"],
-    )
-    dish.save()
+    t = row["Продукт"]
+    if isinstance(t, str) and t:
+        dish = Dish(
+            title=row["Продукт"][6:],
+            grams=row["Вес, г"],
+            calories=row["Калорийность, ккал"],
+            protein=row["Белок, г"],
+            fats=row["Жир общий, г"],
+            carbohydrates=row["Углеводы, по разности, г"],
+        )
+        dish.save()
